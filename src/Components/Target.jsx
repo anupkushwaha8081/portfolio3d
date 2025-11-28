3a929fc6255a9c1b7d1d8b3d917e96af7b73f3dc
Anup Kushwaha <125217613+anupkushwaha8081@users.noreply.github.com>
2025-11-28 20:23:10 +0530

    update target gltf

--- src/Components/Target.jsx
+++ src/Components/Target.jsx
@@ -27,9 +27,11 @@ import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
+  // const { scene } = useGLTF(
+  //   'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
+  // );
+  const { scene } = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf");
+
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
